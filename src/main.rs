6381a4abb4582a0c5e4498246e79336ec119a001
Alexander Grube <alexp.grube@googlemail.com>
2023-03-23 18:56:00 +0100

    secret basic logic implemented

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, middleware::Logger, web, post, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder, http::header::ContentType};
 use env_logger::Env;
 extern crate redis;
 use dotenv::dotenv;
@@ -8,9 +8,24 @@ mod model;
 
 use model::SecretRequest;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+#[get("/secret/{uuid}")]
+async fn get_and_delete_secret(
+    path: web::Path<String>,
+    redis: web::Data<redis::Client>) -> impl Responder {
+    let uuid = path.into_inner();
+    let mut connection = redis.get_connection().unwrap();
+    let data: Option<String> = connection.get(&uuid).or_else(|e| {
+        println!("Error: {:?}", e);
+        Err(e)
+    }).unwrap();
+    if data.is_some() {
+        let _: () = connection.del(&uuid).or_else(|e| {
+            println!("Error: {:?}", e);
+            Err(e)
+        }).unwrap();
+        return HttpResponse::Ok().content_type(ContentType::json()).body(format!("{{\"data\": \"{}\"}}", data.unwrap()));
+    }
+    HttpResponse::NotFound().content_type(ContentType::json()).body(format!("{{\"error\": \"not found\"}}"))
 }
 
 #[post("/secret")]
@@ -18,14 +33,16 @@ async fn create_secret(
     web::Json(secret): web::Json<SecretRequest>,
     redis: web::Data<redis::Client>,
 ) -> impl Responder {
-        let uuid = uuid::Uuid::new_v4();
-        let mut connection = redis.get_connection().unwrap();
-        let _: () = connection.set_ex(uuid.to_string(), secret.data, secret.ttl).or_else(|e| {
+    let uuid = uuid::Uuid::new_v4();
+    let mut connection = redis.get_connection().unwrap();
+    let _: () = connection
+        .set_ex(uuid.to_string(), secret.data, secret.ttl)
+        .or_else(|e| {
             println!("Error: {:?}", e);
             Err(e)
-        }).unwrap();
-        HttpResponse::Ok().body(format!("{{\"uuid\": \"{}\"}}", uuid))
-        
+        })
+        .unwrap();
+    HttpResponse::Ok().content_type(ContentType::json()).body(format!("{{\"uuid\": \"{}\"}}", uuid))
 }
 
 #[actix_web::main]
@@ -37,9 +54,9 @@ async fn main() -> std::io::Result<()> {
     let redis = redis::Client::open(db_connection_string).unwrap();
     HttpServer::new(move || {
         App::new()
-        .app_data(web::Data::new(redis.clone()))
+            .app_data(web::Data::new(redis.clone()))
             .wrap(Logger::default())
-            .service(hello)
+            .service(get_and_delete_secret)
             .service(create_secret)
     })
     .bind(("127.0.0.1", 8080))?
